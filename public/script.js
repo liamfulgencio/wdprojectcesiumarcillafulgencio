446e320b1650a532c88ea15f4652314c71ed883f
Glitch (glitch-hello-node) <none>
2024-12-23 08:40:18 +0000

    🏦📫 Checkpoint ./src/pages/index.hbs:70058461/5793 ./public/style.css:70058461/5213 ./public/script.js:70058461/1005 ./public/page3.html:70058461/1594 ./public/page2.html:70058461/1814 ./public/page1.html:70058461/645

---
+++ public/script.js
@@ -0,0 +1,43 @@
+function redirection(page) {
+  switch (page){
+    case 1: 
+      window.location.href = 'index.hbs';
+      break;
+    case 2:
+      window.location.href = 'page2.html';
+      break;
+    case 3:
+      window.location.href = 'page3.html';
+      break;
+    case 4: 
+      window.location.href = 'page4.html';
+      break;
+    case 5:
+      window.location.href = 'page5.html';
+      break;
+    case 6:
+      window.location.href = 'page6.html';
+      break;
+    case 7: 
+      window.location.href = 'page7.html';
+      break;
+    case 8:
+      window.location.href = 'page8.html';
+      break;
+    case 9:
+      window.location.href = 'page9.html';
+      break;
+    case 10: 
+      window.location.href = 'page10.html';
+      break;
+    case 11:
+      window.location.href = 'pag11.html';
+      break;
+    case 12:
+      window.location.href = 'page12.html';
+      break;
+    case 13:
+      window.location.href = 'page13.html';
+      break;
+  }
+}
